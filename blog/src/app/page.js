f8a7091f0fedbffb6ae1755f766c24fafd41c0a5
tanushree-deloitte <tanushree7@deloitte.com>
2023-08-07 09:45:33 +0530

    Reuse Component

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <User />
+      <User />
+      <User />
       <User />
       <h1>Home page</h1>
     </main>
