77d32c5b332f873392666072dc32140bec97af3b
tanushree-deloitte <tanushree7@deloitte.com>
2023-08-07 09:48:37 +0530

    Data passing in components

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -3,19 +3,17 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <User />
-      <User />
-      <User />
-      <User />
+      <User name="Tanushree" />
+      <User name="Tanu" />
       <h1>Home page</h1>
     </main>
   );
 }
 
-const User = () => {
+const User = (props) => {
   return (
     <div>
-      <h2>Tanushree</h2>
+      <h2>{props.name}</h2>
     </div>
   );
 };
