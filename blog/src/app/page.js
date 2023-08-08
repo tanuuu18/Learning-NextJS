8f32af25cd3c8f32aeacd0457c4ffda84b670431
tanushree-deloitte <tanushree7@deloitte.com>
2023-08-09 00:21:56 +0530

    Functions in nextJs

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -2,10 +2,13 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  const apple = () => {
+    alert("Fruit");
+  };
   return (
     <main className={styles.main}>
       <h1>Events, Function and State</h1>
-      <button onClick={() => alert("Hello Next.js")}>Click me</button>
+      <button onClick={apple}>Click me</button>
     </main>
   );
 }
