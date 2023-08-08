cf7628d43f28f1a11f1f7135cebc2f1264e247cb
tanushree-deloitte <tanushree7@deloitte.com>
2023-08-09 00:33:40 +0530

    Variable doesn't re-render

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -4,12 +4,13 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const [name, setName] = useState("Tanu");
+  let data = "T";
   const apple = () => {
-    setName("Tanushree");
+    data = "t";
   };
   return (
     <main className={styles.main}>
-      <h1>Events, Function and State {name}</h1>
+      <h1>Events, Function and State {data}</h1>
       <button onClick={() => apple()}>Click me</button>
     </main>
   );
