d42742e81d3f79408e4e3efddda645b10d3e4635
tanushree-deloitte <tanushree7@deloitte.com>
2023-08-09 00:43:43 +0530

    Functions

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -4,14 +4,23 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const [name, setName] = useState("Tanu");
-  let data = "T";
+
   const apple = () => {
-    data = "t";
+    setName("Tanushree");
+  };
+
+  const InnerComp = () => {
+    return <h1>Inner Component</h1>;
   };
   return (
     <main className={styles.main}>
-      <h1>Events, Function and State {data}</h1>
+      <h1>Events, Function and State {name}</h1>
       <button onClick={() => apple()}>Click me</button>
+      {/* Component calling as a function
+      Calling function as a Component */}
+      <InnerComp />
+      {/* Using as a function */}
+      {InnerComp()}
     </main>
   );
 }
